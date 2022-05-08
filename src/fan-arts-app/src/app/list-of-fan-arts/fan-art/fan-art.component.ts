2acd7d668b175f9d0fa2b9241193e890e66660a9
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-08 10:33:22 +0300

    Add fan-art component

---
+++ src/fan-arts-app/src/app/list-of-fan-arts/fan-art/fan-art.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fan-art',
+  templateUrl: './fan-art.component.html',
+  styleUrls: ['./fan-art.component.css']
+})
+export class FanArtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
