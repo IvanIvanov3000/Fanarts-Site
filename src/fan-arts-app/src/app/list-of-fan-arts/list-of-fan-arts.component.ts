947c7dbd12c2c20350f8993987c9fab28f4bd6b9
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-08 16:51:30 +0300

    css logic

--- src/fan-arts-app/src/app/list-of-fan-arts/list-of-fan-arts.component.ts
+++ src/fan-arts-app/src/app/list-of-fan-arts/list-of-fan-arts.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {FanArt} from '../fanArts'
+
 
 @Component({
   selector: 'app-list-of-fan-arts',
   templateUrl: './list-of-fan-arts.component.html',
   styleUrls: ['./list-of-fan-arts.component.css']
 })
-export class ListOfFanArtsComponent implements OnInit {
 
+
+export class ListOfFanArtsComponent implements OnInit {
+  @Input() theme!: string;
+  @Input() fanArts! : FanArt[]; 
   constructor() { }
 
   ngOnInit(): void {
