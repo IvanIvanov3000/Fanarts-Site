c9f04295400e8e4b72b3cfcc7901fe0ae89d7565
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-08 16:48:53 +0300

    Add some routing links

--- src/fan-arts-app/src/app/app-routing.module.ts
+++ src/fan-arts-app/src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import {HomePageComponent}   from './home-page/home-page.component';
+import {AboutPageComponent}   from './about-page/about-page.component';
+import {CatalogPageComponent}   from './catalog-page/catalog-page.component';
 import {LoginPageComponent}   from './auth-pages/login-page/login-page.component';
-
+import {RegisterPageComponent}   from './auth-pages/register-page/register-page.component';
  
 const routes: Routes = [
   {path: '', component: HomePageComponent},
   {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: 'catalog', component: CatalogPageComponent},
 ];
 
 @NgModule({
