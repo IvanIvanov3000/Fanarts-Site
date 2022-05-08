ea8cc142891326c50fa8ccd0043709156e8d0e8b
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-08 10:42:02 +0300

    Add routes for home and login page

--- src/fan-arts-app/src/app/app-routing.module.ts
+++ src/fan-arts-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {HomePageComponent}   from './home-page/home-page.component';
+import {LoginPageComponent}   from './auth-pages/login-page/login-page.component';
+
+ 
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
