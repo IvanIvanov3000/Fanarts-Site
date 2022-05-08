4316d3ff6b6265a8514bf672770cca1e96d82c66
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-08 10:42:55 +0300

    Import all created components

--- src/fan-arts-app/src/app/app.module.ts
+++ src/fan-arts-app/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './auth-pages/login-page/login-page.component';
+import { HomeHeroComponent } from './home-page/home-hero/home-hero.component';
+import { ListOfFanArtsComponent } from './list-of-fan-arts/list-of-fan-arts.component';
+import { FanArtComponent } from './list-of-fan-arts/fan-art/fan-art.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    HomeHeroComponent,
+    ListOfFanArtsComponent,
+    FanArtComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
