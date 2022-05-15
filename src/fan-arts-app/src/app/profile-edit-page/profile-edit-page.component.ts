8a7ef7b2b586df8cd6cd55c737208f13a3341942
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-15 13:08:01 +0300

    Add profile-edit page

---
+++ src/fan-arts-app/src/app/profile-edit-page/profile-edit-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-edit-page',
+  templateUrl: './profile-edit-page.component.html',
+  styleUrls: ['./profile-edit-page.component.css']
+})
+export class ProfileEditPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
