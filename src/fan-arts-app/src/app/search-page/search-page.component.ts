1aef999e1bfc3fdf4310cc496d53c9edbbc7f0fc
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-15 13:08:35 +0300

    Add search-page component

---
+++ src/fan-arts-app/src/app/search-page/search-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { example, FanArt } from '../fanArts';
+
+@Component({
+  selector: 'app-search-page',
+  templateUrl: './search-page.component.html',
+  styleUrls: ['./search-page.component.css']
+})
+export class SearchPageComponent implements OnInit {
+
+  theme = "blue"
+  fanArts: FanArt[] = example
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
