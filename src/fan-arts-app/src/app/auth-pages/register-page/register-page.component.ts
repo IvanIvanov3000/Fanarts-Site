f49f8467fb1fef222bf76c038ee1e06f84901152
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-18 20:01:05 +0300

    Update register-page.component.ts so that it handles validation

--- src/fan-arts-app/src/app/auth-pages/register-page/register-page.component.ts
+++ src/fan-arts-app/src/app/auth-pages/register-page/register-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+
+import { AuthService } from '../../_services/auth.service';
+
 
 @Component({
   selector: 'app-register-page',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent implements OnInit {
+  form: any = {
+    username: null,
+    email: null,
+    password: null,
+    rePassword: null
+  };
+  passwordsMatch = false;
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+  onSubmit(): void {
+    const { username, email, password } = this.form;
+
+    this.authService.register(username, email, password).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
   }
+
 
 }
