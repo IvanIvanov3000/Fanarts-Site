4dde27c21de6bcc99c314bd89243c7912535dd73
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-20 17:25:31 +0300

    Sort the different components into types with similarities

--- src/fan-arts-app/src/app/app.module.ts
+++ src/fan-arts-app/src/app/app.module.ts
@@ -8,20 +8,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { LoginPageComponent } from './auth-pages/login-page/login-page.component';
+
 import { HomeHeroComponent } from './home-page/home-hero/home-hero.component';
-import { ListOfFanArtsComponent } from './list-of-fan-arts/list-of-fan-arts.component';
-import { FanArtComponent } from './list-of-fan-arts/fan-art/fan-art.component';
-import { RegisterPageComponent } from './auth-pages/register-page/register-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
-import { CatalogPageComponent } from './catalog-page/catalog-page.component';
-import { CreatePageComponent } from './create-page/create-page.component';
-import { DetailsEditPageComponent } from './details-edit-page/details-edit-page.component';
-import { DetailsPageComponent } from './details-page/details-page.component';
-import { MyPostsPageComponent } from './my-posts-page/my-posts-page.component';
+
+import { LoginPageComponent } from './auth-pages/login-page/login-page.component';
+import { RegisterPageComponent } from './auth-pages/register-page/register-page.component';
 import { ProfilePageComponent } from './auth-pages/profile-page/profile-page.component';
 import { ProfileEditPageComponent } from './auth-pages/profile-edit-page/profile-edit-page.component';
+import { MyPostsPageComponent } from './my-posts-page/my-posts-page.component';
+
+import { ListOfFanArtsComponent } from './list-of-fan-arts/list-of-fan-arts.component';
+import { FanArtComponent } from './list-of-fan-arts/fan-art/fan-art.component';
+
+
+import { CreatePageComponent } from './create-page/create-page.component';
+import { CatalogPageComponent } from './catalog-page/catalog-page.component';
+
+import { DetailsPageComponent } from './details-page/details-page.component';
+import { DetailsEditPageComponent } from './details-edit-page/details-edit-page.component';
 import { SearchPageComponent } from './search-page/search-page.component';
 
 import { PasswordvalidatorDirective } from './auth-pages/register-page/passwordvalidator.directive';
