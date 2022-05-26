bbb8f3722cdb52d45d54bee1308c2d367ced411e
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-26 19:49:22 +0300

    Organise the different routes in app-routing.module

--- src/fan-arts-app/src/app/app-routing.module.ts
+++ src/fan-arts-app/src/app/app-routing.module.ts
@@ -15,21 +15,23 @@ import { ProfilePageComponent } from './auth-pages/profile-page/profile-page.com
 import { ProfileEditPageComponent } from './auth-pages/profile-edit-page/profile-edit-page.component';
 import { SearchPageComponent } from './search-page/search-page.component';
 
-
+import { AuthGuardService } from './auth-pages/auth-guard.service';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'search', component: SearchPageComponent },
+  { path: 'catalog', component: CatalogPageComponent },
+
+  { path: 'details', component: DetailsPageComponent },
+  { path: 'details/edit', component: DetailsEditPageComponent, canActivate: [AuthGuardService] },
+  { path: 'create', component: CreatePageComponent, canActivate: [AuthGuardService] },
+
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
-  { path: 'about', component: AboutPageComponent },
-  { path: 'catalog', component: CatalogPageComponent },
-  { path: 'create', component: CreatePageComponent },
-  { path: 'details', component: DetailsPageComponent },
-  { path: 'details/edit', component: DetailsEditPageComponent },
-  { path: 'myposts', component: MyPostsPageComponent },
-  { path: 'profile', component: ProfilePageComponent },
-  { path: 'profile/edit', component: ProfileEditPageComponent },
-  { path: 'search', component: SearchPageComponent },
+  { path: 'myposts', component: MyPostsPageComponent, canActivate: [AuthGuardService] },
+  { path: 'profile', component: ProfilePageComponent, canActivate: [AuthGuardService] },
+  { path: 'profile/edit', component: ProfileEditPageComponent, canActivate: [AuthGuardService] },
 
 
 ];
