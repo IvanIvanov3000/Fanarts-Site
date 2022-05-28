9bc898d230ef032844065309c263c62cc064dd92
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-28 17:55:50 +0300

    Add router navigation to footer.component and change the logout anchor tag to button tag

--- src/fan-arts-app/src/app/footer/footer.component.ts
+++ src/fan-arts-app/src/app/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { TokenStorageService } from '../_services/token-storage.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  isLoggedIn = false;
 
-  constructor() { }
+  constructor(private tokenStorage: TokenStorageService, public router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const token = this.tokenStorage.getToken();
+
+    if (token) {
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+    }
+  }
+
+  onLogout() {
+    this.tokenStorage.logOut();
+    return window.location.reload();
+
   }
 
 }
