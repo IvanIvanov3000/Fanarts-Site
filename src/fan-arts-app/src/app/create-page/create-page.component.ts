80fae1fd022d773ffe362c322e07a094b3d0188b
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-29 09:25:47 +0300

    Add the fanArtsService to create-page.ts and make a create request

--- src/fan-arts-app/src/app/create-page/create-page.component.ts
+++ src/fan-arts-app/src/app/create-page/create-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { FanArtsService } from '../_services/fan-arts.service';
 
 @Component({
   selector: 'app-create-page',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePageComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    title: null,
+    image: null,
+    isPublic: null,
+    tag: null,
+    description: null,
 
-  ngOnInit(): void {
+  };
+  isSuccessful = false;
+  isCreateFailed = false;
+  errorMessage = '';
+
+  constructor(private fanArtsService: FanArtsService, public router: Router) { }
+
+  ngOnInit(): void { }
+
+  onSubmit(): void {
+    const { title, image, isPublic, tag, description } = this.form;
+
+    console.log(title, image, isPublic, tag, description);
+    this.fanArtsService.create(title, image, isPublic, tag, description)
+      .subscribe(
+        data => {
+          console.log(data);
+          setTimeout(() => {
+            this.router.navigate(['/']);
+            console.log("Delayed for 0.5 second.");
+          }, 500);
+
+          setTimeout(() => {
+            this.reloadPage();
+
+            console.log("Delayed for 2 second.");
+          }, 2000);
+        },
+        err => {
+          console.log(err);
+          this.errorMessage = err.error.message;
+        }
+      );
+  }
+  reloadPage(): void {
+    window.location.reload();
   }
 
 }
