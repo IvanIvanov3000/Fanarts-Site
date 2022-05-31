a08c58c190c738966f6b8a5637033e73bcd2ed50
Ivan <ivan_ivanov_3000@abv.bg>
2022-05-31 17:49:09 +0300

    Make the details-page.ts make a getFanArt request and details-page.html display the information

--- src/fan-arts-app/src/app/details-page/details-page.component.ts
+++ src/fan-arts-app/src/app/details-page/details-page.component.ts
@@ -1,4 +1,14 @@
+//import { switchMap } from 'rxjs/operators';
+
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+//import { Observable } from 'rxjs';
+
+import { FanArt } from "../fanArts";
+import { FanArtsService } from '../_services/fan-arts.service';
+import { TokenStorageService } from '../_services/token-storage.service';
+
+
 
 @Component({
   selector: 'app-details-page',
@@ -7,9 +17,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPageComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  fanArt: FanArt = {
+    _id: 1,
+    title: '',
+    isPublic: true,
+    image: '',
+    tag: '',
+    description: '',
+    author: ''
   }
+  isOwner: boolean = false;
+  show!: boolean;
+
+  constructor(
+    private fanArtsService: FanArtsService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private tokenService: TokenStorageService,
+  ) { }
+
+  ngOnInit() {
+    // this.fanArt$ = this.route.paramMap.pipe(
+    //   switchMap((params: ParamMap) => {
+
+    //     console.log(params.get('id'));
+    //     return this.fanArtsService.getFanArt(params.get('id')!);
+    //   }));
+    const currentUser = this.tokenService.getUser();
+
+    let id = this.route.snapshot.params['id'];
+    this.fanArtsService.getFanArt(id).subscribe(
+      data => {
+        this.fanArt = data;
+        let author = data.author._id;
+        let id = currentUser.id;
+
+        if (author == id) {
+          this.isOwner = true;
+        } else {
+          this.isOwner = false;
+        }
+        this.show = this.fanArt.isPublic;
+      },
+      err => {
+        console.log(err);
+      });
+
+
+  }
+
 
 }
