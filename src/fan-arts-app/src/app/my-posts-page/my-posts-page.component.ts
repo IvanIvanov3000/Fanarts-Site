43e06aff586353335562592904f66e206ab698ba
Ivan <79756968+IvanIvanov3000@users.noreply.github.com>
2022-06-05 18:05:30 +0300

    Update my-posts-page.component.ts

--- src/fan-arts-app/src/app/my-posts-page/my-posts-page.component.ts
+++ src/fan-arts-app/src/app/my-posts-page/my-posts-page.component.ts
@@ -1,7 +1,8 @@
+@@ -0,0 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
-import { example, FanArt } from '../fanArts';
-
+import { FanArt } from '../fanArts';
+import { FanArtsService } from '../_services/fan-arts.service';
 @Component({
   selector: 'app-my-posts-page',
   templateUrl: './my-posts-page.component.html',
@@ -9,12 +10,26 @@ import { example, FanArt } from '../fanArts';
 })
 export class MyPostsPageComponent implements OnInit {
 
-
+  areThereFanArts: boolean = false;
   theme = "orange"
   fanArts!: FanArt[];
-  constructor() { }
+  constructor(private fanArtsService: FanArtsService) { }
 
   ngOnInit(): void {
+    this.fanArtsService.getMyFanArts().subscribe(
+      data => {
+        this.fanArts = data;
+        if (data.length > 0) {
+          this.areThereFanArts = true;
+        } else {
+          this.areThereFanArts = false;
+        }
+        console.log(this.areThereFanArts);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
 }
