85512f027ee8c280ad7d1392c8b897c4907039cd
Ivan <79756968+IvanIvanov3000@users.noreply.github.com>
2022-06-05 17:43:13 +0300

    Update about-page.component.ts

--- src/fan-arts-app/src/app/about-page/about-page.component.ts
+++ src/fan-arts-app/src/app/about-page/about-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TokenStorageService } from '../_services/token-storage.service'
+
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPageComponent implements OnInit {
 
-  constructor() { }
+  audio!: any;
+  isLoggedIn: boolean = false;
+  constructor(private tokenService: TokenStorageService) { }
 
   ngOnInit(): void {
-  }
 
+    let audio = new Audio();
+    audio.src = "../../assets/Images/about-page/pikachusound.mp3";
+    this.audio = audio;
+    let token = this.tokenService.getToken();
+    console.log(token);
+    if(token){
+      this.isLoggedIn = true;
+    }else{
+      this.isLoggedIn = false;
+    }
+
+  }
+  playAudio() {
+    this.audio.play();
+
+  }
 }
